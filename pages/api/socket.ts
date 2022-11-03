d75b6b935be1949e3bf44fd11334e45e36d6de56
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-11-03 22:52:36 +0900

    api socket update

--- pages/api/socket.ts
+++ pages/api/socket.ts
@@ -53,6 +53,9 @@ export default function socketIoHanddler(
       SocketData
     >(res.socket.server, {
       path: "/api/socket",
+      cors: {
+        origin: "https://chat-app-zpdl95.vercel.app",
+      },
     });
     res.socket.server.io = socketIoServer;
 
