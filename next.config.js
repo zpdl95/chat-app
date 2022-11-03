ace919085e3b293137655d188c7d205a7f1b694a
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-11-03 19:22:40 +0900

    next config update

--- next.config.js
+++ next.config.js
@@ -1,15 +1,18 @@
 /** @type {import('next').NextConfig} */
 
-const repository = "https://chat-app-kappa-flame.vercel.app/";
+const repository =
+  process.env.NODE_ENV == "production"
+    ? "https://chat-app-kappa-flame.vercel.app"
+    : "";
 
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
-  images: {
-    loader: "imgix",
-    path: [repository],
-    // domain: [repository], // 로컬파일을 사용하면 path, 외부파일을 사용하면 domain
-  },
+  // images: {
+  //   loader: "imgix",
+  //   // path: [repository],
+  //   // domain: [repository], // 로컬파일을 사용하면 path, 외부파일을 사용하면 domain
+  // },
   exportPathMap: async function (
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
