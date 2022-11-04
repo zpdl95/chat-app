2cf2628079ab1985a9d5dd6523d0ffcfb22d43d5
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-11-04 20:04:58 +0900

    tailwind.config update3

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   content: [
     join(__dirname, "./app/**/*.{js,ts,jsx,tsx}"),
     join(__dirname, "./pages/**/*.{js,ts,jsx,tsx}"),
-    join(__dirname, "./components/**/*.{js,ts,jsx,tsx}"),
+    join(__dirname, "./Components/**/*.{js,ts,jsx,tsx}"),
   ],
   theme: {
     extend: {
