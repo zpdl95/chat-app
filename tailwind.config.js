c6870d48924c7277d1d6b3ada567cef696e9e58f
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-11-04 19:52:54 +0900

    tailwind.css config update2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 const plugin = require("tailwindcss/plugin");
+const { join } = require("path");
 
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    join(__dirname, "./app/**/*.{js,ts,jsx,tsx}"),
+    join(__dirname, "./pages/**/*.{js,ts,jsx,tsx}"),
+    join(__dirname, "./components/**/*.{js,ts,jsx,tsx}"),
   ],
   theme: {
     extend: {
